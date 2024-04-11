cd5954d58696ed12f347f74f40cc1c4be68de2b3
Pame05 <146900728+Pame05@users.noreply.github.com>
2024-04-11 16:44:16 -0500

    expplicacion exercisms

--- semana1/exercism/2-TwoFer.ts
+++ semana1/exercism/2-TwoFer.ts
@@ -1,3 +1,8 @@
 export function twoFer(name?:string): string {
     return `One for ${name || 'you'}, one for me.`
   }
+
+  // El codigo recibe name el cual es opcional y de tipo string y devuelve
+  // One for el name colocado, one for me. En el 
+  // caso que no se proporcione ningun nombre devolvera, One for you,
+  // one for me.
--- semana1/exercism/3-ResistorColor.ts
+++ semana1/exercism/3-ResistorColor.ts
@@ -4,3 +4,7 @@ export const colorCode = (color:string) => {
   return COLORS.indexOf(color);
 };
                        
+// Primero se crea un array que contenga los colores del código de colores de las resistencias
+// Luego, se realiza un funcion y se declara el tipo de dato de color con el fin de obtener
+//   como retorno cada valor de los COLORES. Esto se lo realiza con el indexOf el cual muestra 
+//   el indice de cada elemento del array , que concuerda con el valor de cada color.
